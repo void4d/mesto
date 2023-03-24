f14451c73fdea265c627ff0847c0b57534ae2591
void4d <tulubaevp@icloud.com>
2023-03-24 23:57:15 +0500

    feat: Добавить JS к всплывающему окну

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close-button');
+const saveButton = document.querySelector('.popup__save-button');
+
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__description');
+let popupForm = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = popupForm.querySelector('.popup__input_type_description');
+
+
+editButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+  nameInput.value = nameProfile.innerHTML;
+  jobInput.value = jobProfile.innerHTML;
+});
+
+editButton.addEventListener('click', function () {
+  popup.classList.remove('popup_closed');
+});
+
+closeButton.addEventListener('click', function () {
+  popup.classList.add('popup_closed');
+})
+
+popupForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  nameProfile.innerHTML = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  popup.classList.add('popup_closed');
+})
