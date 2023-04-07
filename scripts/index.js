92a1c19ad84dc87d9def92098d92444fdde6f610
void4d <tulubaevp@icloud.com>
2023-04-07 16:25:24 +0500

    fix: Добавить открытие окна просмотра изображения

--- scripts/index.js
+++ scripts/index.js
@@ -11,10 +11,17 @@ const jobInputEdit = popupProfileEdit.querySelector('.popup__input_type_descript
 // Окно добавления карточки
 const popupAddCard = document.querySelector('.popup__type_add-card');
 
+// Окно просмотра полного изображения
+const popupOpenCard = document.querySelector('.popup__type_open-card');
+
+// Фото карточки
+const cardPhoto = document.querySelector('.elements__photo');
+
 // Кнопки
 const editButton = document.querySelector('.profile__edit-button');
 const closeButtonEdit = popupProfileEdit.querySelector('.popup__close-button');
 const closeButtonAddCard = popupAddCard.querySelector('.popup__close-button');
+const closeButtonOpenCard = popupOpenCard.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__save-button');
 const likeButton = document.querySelector('.elements__like-button');
 const addButton = document.querySelector('.profile__add-button');
@@ -29,7 +36,7 @@ editButton.addEventListener('click', function () {
 // Закрытие окна редактирования профиля
 closeButtonEdit.addEventListener('click', function () {
   popupProfileEdit.classList.remove('popup_opened');
-})
+});
 
 // Открытие окна добавления карточки
 addButton.addEventListener('click', function () {
@@ -39,7 +46,17 @@ addButton.addEventListener('click', function () {
 // Закрытие окна добавления карточки
 closeButtonAddCard.addEventListener('click', function () {
   popupAddCard.classList.remove('popup_opened');
-})
+});
+
+// Открытие окна просмотра полного изображения
+cardPhoto.addEventListener('click', function () {
+  popupOpenCard.classList.add('popup_opened');
+});
+
+// Закрытие окна просмотра полного изображения
+closeButtonOpenCard.addEventListener('click', function () {
+  popupOpenCard.classList.remove('popup_opened');
+});
 
 // Сабмит окна редактирования профиля
 popupFormEdit.addEventListener('submit', function (event) {
@@ -47,8 +64,9 @@ popupFormEdit.addEventListener('submit', function (event) {
   nameProfile.textContent = nameInputEdit.value;
   jobProfile.textContent = jobInputEdit.value;
   popupProfileEdit.classList.remove('popup_opened');
-})
+});
 
+// Кнопка лайка
 likeButton.addEventListener('click', function () {
   likeButton.classList.toggle('elements__like-button_active');
 });
