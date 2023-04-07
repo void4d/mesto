de40651a5ff4153017d518282bfb226fdf9cd39a
void4d <tulubaevp@icloud.com>
2023-04-07 15:57:46 +0500

    fix: Добавить закрытие окну добавления

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,16 @@ const popupFormEdit = popupProfileEdit.querySelector('.popup__form');
 const nameInputEdit = popupProfileEdit.querySelector('.popup__input_type_name');
 const jobInputEdit = popupProfileEdit.querySelector('.popup__input_type_description');
 
+// Окно добавления карточки
+const popupAddCard = document.querySelector('.popup__type_add-card');
+
 // Кнопки
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+const closeButtonEdit = popupProfileEdit.querySelector('.popup__close-button');
+const closeButtonAddCard = popupAddCard.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__save-button');
 const likeButton = document.querySelector('.elements__like-button');
+const addButton = document.querySelector('.profile__add-button');
 
 // Открытие окна редактирования профиля
 editButton.addEventListener('click', function () {
@@ -21,9 +26,19 @@ editButton.addEventListener('click', function () {
   jobInputEdit.value = jobProfile.textContent;
 });
 
-// Кнопка закрытия модального окна
-closeButton.addEventListener('click', function () {
+// Закрытие окна редактирования профиля
+closeButtonEdit.addEventListener('click', function () {
   popupProfileEdit.classList.remove('popup_opened');
+})
+
+// Открытие окна добавления карточки
+addButton.addEventListener('click', function () {
+  popupAddCard.classList.add('popup_opened');
+});
+
+// Закрытие окна добавления карточки
+closeButtonAddCard.addEventListener('click', function () {
+  popupAddCard.classList.remove('popup_opened');
 })
 
 // Сабмит окна редактирования профиля
