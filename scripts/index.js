ff4bd0a0b4ff4c72f4c7d55ff9ff948c90cba104
void4d <tulubaevp@icloud.com>
2023-04-21 15:00:38 +0500

    Добавить валидацию форм

--- scripts/index.js
+++ scripts/index.js
@@ -120,18 +120,18 @@ closeButtonEdit.addEventListener('click', function () {
 });
 
 // Закрытие попапов на Esc
-function closePopupByEsc (evt) {
+function closePopupByEsc(evt) {
   if (evt.key === 'Escape') {
-   closePopup(popupProfileEdit);
-   closePopup(popupOpenCard);
-   closePopup(popupAddCard);
+    closePopup(popupProfileEdit);
+    closePopup(popupOpenCard);
+    closePopup(popupAddCard);
   };
 };
 
 document.addEventListener('keydown', closePopupByEsc);
 
 // Закрытие попапов на клик по оверлею
-function closePopupByClickOnOverlay (evt) {
+function closePopupByClickOnOverlay(evt) {
   if (evt.target == overlay) {
     closePopup(popupProfileEdit);
     closePopup(popupOpenCard);
@@ -176,5 +176,57 @@ function cardSubmit(event) {
 popupAddForm.addEventListener('submit', cardSubmit);
 
 
+// Валидация формы
 
+function setInputValidState(input, errorMessage) {
+  saveButton.classList.remove('popup__save-button_disabled');
+  saveButton.removeAttribute('disabled', true);
+  input.classList.remove('popup__input_invalid');
+  errorMessage.textContent = '';
+};
 
+function setInputInvalidState(input, errorMessage) {
+  saveButton.classList.add('popup__save-button_disabled');
+  saveButton.setAttribute('disabled', true);
+  input.classList.add('popup__input_invalid');
+  console.log(errorMessage);
+  errorMessage.textContent = input.validationMessage;
+};
+
+function checkInputValidity(input) {
+  const errorMessage = popupContainer.querySelector(`#${input.id}-error`);
+
+  if (input.validity.valid) {
+    setInputValidState(input, errorMessage);
+  } else {
+    setInputInvalidState(input, errorMessage);
+  }
+};
+
+function enableEditFormValidation() {
+  const EditForm = document.querySelector('#editform');
+  const EditFormInputs = EditForm.querySelectorAll('.popup__input')
+  const EditFormInputsArray = Array.from(EditFormInputs);
+
+  EditFormInputsArray.forEach(function (input) {
+    input.addEventListener('input', function() {
+      checkInputValidity(input);
+    });
+  });
+};
+
+enableEditFormValidation();
+
+function enableAddFormValidation() {
+  const AddForm = document.querySelector('#addform');
+  const AddFormInputs = AddForm.querySelectorAll('.popup__input');
+  const AddFormInputsArray = Array.from(AddFormInputs);
+
+  AddFormInputsArray.forEach(function (input) {
+    input.addEventListener('input', function() {
+      checkInputValidity(input);
+    });
+  });
+};
+
+enableAddFormValidation();
